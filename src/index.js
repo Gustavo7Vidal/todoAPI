60dbe75637d802a9803df22be08427f880ade2a1
Gustavo7Vidal <vidalgitgustavo@gmail.com>
2023-03-14 15:15:53 -0300

    adicionando get e post na todo list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
@@ -7,35 +7,69 @@ app.use(express.json());
 
 const users = [];
 
-function checkExistsUserAccount(req, res, next){
+function checkExistsUserAccount(req, res, next) {
     //
-    const {id} = req;
-    
-    users.filter()
+    const { username } = req.headers;
+
+    const user = users.find((user) => user.username === username)
+
+    if (!user) {
+        return res.status(404).json({ error: 'User not exists' })
+    }
+    req.user = user;
+    return next()
 }
 
-app.post('/users',  (req, res) => {
+app.post('/users', (req, res) => {
     const { name, username } = req.body;
 
-    users.push({
-        id : uuid(),
+    if (users.find((userSearch) => userSearch.username === username)) {
+        return res.status(400).json({ error: 'User already exists' })
+    }
+    const user = {
+        id: uuidv4(),
         name,
         username,
-        todos : []
-    })
+        todos: []
+    }
+    users.push(user)
 
-    console.log(users)
-    return res.status(201).send()
+    return res.status(201).json(user);
 });
 
 app.get('/todos', checkExistsUserAccount, (req, res) => {
+    const { user } = req;
+
+    return res.status(200).json(user.todos);
 
 });
 
+/* 
+    id: uuid
+    title: string
+    done: false
+    deadline: date
+    created_at: date
+*/
 app.post('/todos', checkExistsUserAccount, (req, res) => {
+    const { user } = req;
+    const { title, deadline } = req.body;
 
+    const dataFormat = new Date(deadline + " 00:00");
+
+    const todoUserAdd = {
+        id: uuidv4(),
+        title,
+        done: false,
+        deadline: new Date(dataFormat),
+        created_at: new Date()
+    }
+
+    user.todos.push(todoUserAdd)
+
+    return res.status(201).json(todoUserAdd);
 });
-
+// Continuar daqui ----------------- // ----------------
 app.put('/todos', checkExistsUserAccount, (req, res) => {
 
 });
